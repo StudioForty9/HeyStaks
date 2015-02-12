49240a5286488ea479ba03d96d0a62fadef2d248
Alan Morkan <alan@studioforty9.com>
2015-02-12 18:05:40 +0000

    Fixing search position when there's no pagination URL parameter.

--- js/heystaks/search.js
+++ js/heystaks/search.js
@@ -3,7 +3,11 @@ document.observe("dom:loaded", function() {
         element.observe('click', function (e) {
             e.stop();
             var resultsPerPage = $$('.item').length;
-            var page = parseInt(document.URL.toQueryParams().p);
+			if (typeof document.URL.toQueryParams().p != 'undefined'){
+				var page = parseInt(document.URL.toQueryParams().p);
+			}else{
+				var page = 1;
+			}
             var position = (element.up('li').previousSiblings().size() + 1) + ((page - 1) * resultsPerPage);
             window.location = element.href + '?referer=heystaks_search_results&position=' + position;
         });
