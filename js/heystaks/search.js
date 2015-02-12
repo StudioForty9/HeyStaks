e2905c99464d3233b05e1ce623151b4dff960a31
Alan Morkan <alan@studioforty9.com>
2015-02-12 15:44:16 +0000

    Fixing JS selectors to work more consistently across themes.

--- js/heystaks/search.js
+++ js/heystaks/search.js
@@ -1,8 +1,8 @@
 document.observe("dom:loaded", function() {
-    $$('.category-products ol.products-list a:not(".link-wishlist, .link-compare")').each(function (element) {
+    $$('.category-products .item a:not(".link-wishlist, .link-compare")').each(function (element) {
         element.observe('click', function (e) {
             e.stop();
-            var resultsPerPage = parseInt($$('.toolbar .limiter select option:selected')[0].innerHTML);
+            var resultsPerPage = $$('.item').length;
             var page = parseInt(document.URL.toQueryParams().p);
             var position = (element.up('li').previousSiblings().size() + 1) + (page * resultsPerPage);
             window.location = element.href + '?referer=heystaks_search_results&position=' + position;
